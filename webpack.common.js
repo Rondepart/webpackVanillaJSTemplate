36290d5abe471bfd58321d3584fd8bedeb04a0c4
Mark <125941419+Rondepart@users.noreply.github.com>
2026-08-16 20:56:59 +0800

    Set up modular webpack config with dev/prod builds

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+   entry: {
+    main: './src/index.js',
+   },
+   plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/template.html',
+    }),
+   ],
+   output: {
+    filename: '[name].js',
+    path: path.resolve(import.meta.dirname, 'dist'),
+    clean: true,
+   },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
